d32808e7cf0e4ee1a2af969ff4615e6c90711cc6
Kevin Farst <farst.6@osu.edu>
2012-05-27 15:10:35 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.12'
+
+gem 'pg'
+
+# Use unicorn as the web server
+gem 'unicorn'
+
+# Deploy with Capistrano
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+group :development, :test do
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'factory_girl'
+end
+
+gem "hobo", "= 1.3.0"
+gem "heroku"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MakeThisSoftware::Application.config.secret_token = 'e772fc903e4bc9dc320430ccfc9b0bc0b38e5fb7c3cac785b20de1c00722bbfda7534228f4a06ab658ba8df98b8ec91a41cc10d5583bf7371e910857ff0ec0f7'
